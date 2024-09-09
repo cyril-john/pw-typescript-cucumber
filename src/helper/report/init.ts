f2ff0c0968204fe7099562310a8d35ef1e27af84
cyril-john <cyriljohn21@gmail.com>
2024-09-09 16:06:48 +0530

    added reporting

---
+++ src/helper/report/init.ts
@@ -0,0 +1,8 @@
+import fs = require("fs-extra");
+try {
+    fs.ensureDir("test-results");
+    fs.emptyDir("test-results");
+
+} catch (error) {
+    console.log("Folder not created! " + error);
+}
---
+++ src/helper/report/report.ts
@@ -0,0 +1,30 @@
+import * as os from 'os';
+const report = require("multiple-cucumber-html-reporter");
+
+
+report.generate({
+    jsonDir: "test-results",
+    reportPath: "test-results/reports/",
+    reportName: "Playwright Automation Report",
+    pageTitle: "BookCart App test report",
+    displayDuration: false,
+    metadata: {
+        browser: {
+            name: "chrome",
+            version: 11
+        },
+        device: "This - PC",
+        platform: {
+            name: os.platform(),
+            version:  os.release()
+        },
+    },
+    customData: {
+        title: "Test Info",
+        data: [
+            { label: "Project", value: "Book Cart Application" },
+            { label: "Release", value: "1.2.3" },
+            { label: "Cycle", value: "Smoke-1" }
+        ],
+    },
+});
